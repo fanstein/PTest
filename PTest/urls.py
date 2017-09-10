728d1e819d89be16fa17ae8a34f2439bb85dd607
fanstein <fanfly88@163.com>
2017-09-10 20:03:16 +0800

    9.10

--- PTest/urls.py
+++ PTest/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from server import views as server_views
+
+server_patterns = [
+    url(r'^/', server_views.index),
+    url(r'^index/', server_views.index),
+    url(r'^keyapi/', server_views.key_api),
+]
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^server/', include(server_patterns)),
 ]
--- server/views.py
+++ server/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def key_api(request):
+    username=request.session.get('user','')
+    
+    return render(request, 'key_api.html' )
